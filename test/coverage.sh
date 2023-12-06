7bfb3f7c6686da2f6a0d1cf23bb009a8167351b7
Tomas Baca <klaxalk@gmail.com>
2023-12-06 14:46:49 +0100

    updated coverage generator script

--- test/coverage.sh
+++ test/coverage.sh
@@ -15,7 +15,7 @@ cd build
 
 lcov --capture --directory . --output-file coverage.info
 lcov --remove coverage.info "*/test/*" --output-file coverage.info.removed
-lcov --extract coverage.info.removed "*/mrs_uav_autostart/*" --output-file coverage.info.cleaned
+lcov --extract coverage.info.removed "*/${PACKAGE_NAME}/*" --output-file coverage.info.cleaned
 genhtml -o coverage_html coverage.info.cleaned | tee /tmp/genhtml.log
 
 COVERAGE_PCT=`cat /tmp/genhtml.log | tail -n 1 | awk '{print $2}'`
