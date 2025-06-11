9f3929ebc3624eb0a05f1e1fe7f1fe1a701fa400
Tomas Baca <Tomas Baca>
2025-06-11 15:17:53 +0200

    + topic check test

--- test/run_specific_test.sh
+++ test/run_specific_test.sh
@@ -11,6 +11,6 @@ done
 
 colcon test-result --delete-yes
 
-colcon test --packages-select mrs_uav_autostart --ctest-args -R 'takeoff_trigerred_externally' --event-handlers console_direct+ console_stderr- console_start_end-
+colcon test --packages-select mrs_uav_autostart --ctest-args -R 'takeoff_should_fail_topic' --event-handlers console_direct+ console_stderr- console_start_end-
 
 colcon test-result --all --verbose
